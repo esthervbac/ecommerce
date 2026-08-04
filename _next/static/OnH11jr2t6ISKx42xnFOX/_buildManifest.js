f6d4c4e50defce471a2a01666be6b5d3c7896501
Esther V Bacelar <esther.v.bacelar@gmail.com>
2026-08-04 19:06:10 -0300

    Updates

--- _next/static/OnH11jr2t6ISKx42xnFOX/_buildManifest.js
+++ _next/static/OnH11jr2t6ISKx42xnFOX/_buildManifest.js
@@ -1,12 +1,7 @@
 self.__BUILD_MANIFEST = {
   "__rewrites": {
     "afterFiles": [],
-    "beforeFiles": [
-      {
-        "source": "/ecommerce//_next/:path+",
-        "destination": "/ecommerce/_next/:path+"
-      }
-    ],
+    "beforeFiles": [],
     "fallback": []
   },
   "sortedPages": [
